e0a97eb426842f51d2bce4e35a15c202a9399a2f
zxtm89j1 <zxtm89j1@gmail.com>
2022-07-25 10:28:18 +0800

    changed the change plan to update membership

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -1,25 +1,25 @@
-import { useState } from "react"
-import useAuth from "../hooks/useAuth"
-import useSubscription from "../hooks/useSubscription"
-import { goToBillingPortal } from "../lib/stripe"
-import Loader from "./Loader"
+import { useState } from "react";
+import useAuth from "../hooks/useAuth";
+import useSubscription from "../hooks/useSubscription";
+import { goToBillingPortal } from "../lib/stripe";
+import Loader from "./Loader";
 
 function Membership() {
-    const { user } = useAuth()
-    const subscription = useSubscription(user)
-    const [isBillingLoading, setBillingLoading] = useState(false)
+  const { user } = useAuth();
+  const subscription = useSubscription(user);
+  const [isBillingLoading, setBillingLoading] = useState(false);
 
-    const manageSubscription = () => {
-        if (subscription) {
-          setBillingLoading(true)
-          goToBillingPortal()
-        }
-      }
+  const manageSubscription = () => {
+    if (subscription) {
+      setBillingLoading(true);
+      goToBillingPortal();
+    }
+  };
   return (
     <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0">
-    <div className="space-y-2 py-4">
-            <h4 className="text-lg text-[#b5b5b5]">Membership & Billing</h4>
-            <button
+      <div className="space-y-2 py-4">
+        <h4 className="text-lg text-[#b5b5b5]">Membership & Billing</h4>
+        <button
           disabled={isBillingLoading || !subscription}
           className="h-10 w-3/5 whitespace-nowrap bg-gray-300 py-2 text-sm font-medium text-black shadow-md hover:bg-gray-200 md:w-4/5"
           onClick={manageSubscription}
@@ -27,11 +27,11 @@ function Membership() {
           {isBillingLoading ? (
             <Loader color="dark:fill-[#e50914]" />
           ) : (
-            'Cancel Membership'
+            "Update Membership"
           )}
         </button>
-        </div>
-        <div className="col-span-3">
+      </div>
+      <div className="col-span-3">
         <div className="flex flex-col justify-between border-b border-white/10 py-4 md:flex-row">
           <div>
             <p className="font-medium text-lg">{user?.email}</p>
@@ -47,8 +47,8 @@ function Membership() {
           <div>
             <p>
               {subscription?.cancel_at_period_end
-                ? 'Your membership will end on '
-                : 'Your next billing date is '}
+                ? "Your membership will end on "
+                : "Your next billing date is "}
               {subscription?.current_period_end}
             </p>
           </div>
@@ -61,7 +61,7 @@ function Membership() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Membership
+export default Membership;
--- pages/account.tsx
+++ pages/account.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import useAuth from '../hooks/useAuth'
-import useSubscription from '../hooks/useSubscription'
-import { GetStaticProps } from 'next'
-import { getProducts, Product } from '@stripe/firestore-stripe-payments'
-import payments from '../lib/stripe'
-import Membership from '../components/Membership'
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import useAuth from "../hooks/useAuth";
+import useSubscription from "../hooks/useSubscription";
+import { GetStaticProps } from "next";
+import { getProducts, Product } from "@stripe/firestore-stripe-payments";
+import payments from "../lib/stripe";
+import Membership from "../components/Membership";
 
-interface Props{
-    products: Product[]
+interface Props {
+  products: Product[];
 }
 
-function account({products}: Props) {
-    console.log(products)
-    const {user, logout} = useAuth()
-    const subscription = useSubscription(user)
+function account({ products }: Props) {
+  console.log(products);
+  const { user, logout } = useAuth();
+  const subscription = useSubscription(user);
   return (
     <div>
-        <Head>
+      <Head>
         <title>Account Settings - Sideflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={`bg-[#141414]`}>
-      <Link href="/">
+        <Link href="/">
           <img
             src="./logo-png/sideflix.png"
             width={120}
@@ -38,33 +38,38 @@ function account({products}: Props) {
             className="cursor-pointer rounded"
           />
         </Link>
-        </header>
+      </header>
 
-        <main className="mx-auto max-w-6xl px-5 pt-24 pb-12 transition-all md:px-10">
-            <div>
-               <h1 className='text-3xl md:text-4xl'>Account</h1>
-               <div className="-ml-0.5 flex items-center gap-x-1.5">
-               <img src="./logo-png/membersinceGold.svg" alt="membership icon" className="h-10 w-10" />
+      <main className="mx-auto max-w-6xl px-5 pt-24 pb-12 transition-all md:px-10">
+        <div>
+          <h1 className="text-3xl md:text-4xl">Account</h1>
+          <div className="-ml-0.5 flex items-center gap-x-1.5">
+            <img
+              src="./logo-png/membersinceGold.svg"
+              alt="membership icon"
+              className="h-10 w-10"
+            />
             <p className="text-base font-bold text-[#b5b5b5]">
               Member since {subscription?.created}
             </p>
-               </div>
+          </div>
+        </div>
 
-            </div>
+        <Membership />
 
-             <Membership/>
-
-    
-
-            <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0 md:pb-0 text-lg">
-                <h3>Plan Details</h3>
-                {/* Find the current Plan */}
-                <div className="col-span-2 font-medium">
-                  {products.filter((product)=>product.id === subscription?.product)[0]?.name}
-                </div>
-                <p  className="cursor-pointer text-blue-500 hover:underline md:text-right" >Change Plan</p>
-            </div>
-            <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0">
+        <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0 md:pb-0 text-lg">
+          <h3>Plan Details</h3>
+          {/* Find the current Plan */}
+          <div className="col-span-2 font-medium">
+            {
+              products.filter(
+                (product) => product.id === subscription?.product
+              )[0]?.name
+            }
+          </div>
+          {/* <p  className="cursor-pointer text-blue-500 hover:underline md:text-right" >Change Plan</p> */}
+        </div>
+        <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0">
           <h4 className="text-lg text-[#b5b5b5]">Settings</h4>
           <p
             className="col-span-3 cursor-pointer text-blue-500 hover:underline text-lg"
@@ -73,35 +78,32 @@ function account({products}: Props) {
             Sign out of all devices
           </p>
         </div>
-        </main>
+      </main>
 
-        <footer className="w-100 flex justify-center">
+      <footer className="w-100 flex justify-center">
         <Link href="/">
-         
-        <button className="bg-amber-500 hover:bg-amber-400 text-white text-lg font-bold py-4 px-6 rounded cursor-pointer ease-in duration-200 ">
-          HOME
-        </button>
+          <button className="bg-amber-500 hover:bg-amber-400 text-white text-lg font-bold py-4 px-6 rounded cursor-pointer ease-in duration-200 ">
+            HOME
+          </button>
         </Link>
-        </footer>
-
-
+      </footer>
     </div>
-  )
+  );
 }
 
-export default account
+export default account;
 
 export const getStaticProps: GetStaticProps = async () => {
-    const products = await getProducts(payments, {
-      includePrices: true,
-      activeOnly: true,
-    })
-      .then((res) => res)
-      .catch((error) => console.log(error.message))
-  
-    return {
-      props: {
-        products,
-      },
-    }
-  }
+  const products = await getProducts(payments, {
+    includePrices: true,
+    activeOnly: true,
+  })
+    .then((res) => res)
+    .catch((error) => console.log(error.message));
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
